4f13a212ee6cadc326a87d3aa9af9ecc24b10635
Seokjun Kim <alma@socar.kr>
2017-03-03 16:17:01 +0900

    Remove test emitter

--- producer/index.js
+++ producer/index.js
@@ -1,6 +1,4 @@
-const { createChannel, EXCHANGES } = require('../rabbitmq')
-
-const A_SECOND = 1000
+const { createChannel } = require('../rabbitmq')
 
 const publish = (exchange, routingKey, message, options = {}) => {
   const content = new Buffer(JSON.stringify(message))
@@ -9,22 +7,6 @@ const publish = (exchange, routingKey, message, options = {}) => {
     .then(channel => channel.publish(exchange, routingKey, content, options))
 }
 
-const delayOptions = {
-  headers: {
-    'x-delay': A_SECOND * 5,
-  },
-}
-
-const emit = () => {
-  Promise.all([
-    publish(EXCHANGES.DELAYED, '', { type: 'x-delayed-message' }, delayOptions),
-    publish(EXCHANGES.DIRECT, '', { type: 'direct' }),
-  ])
-  .then(() => console.info('RabbitMQ: Test messages emitted!'))
-  .catch(e => console.error(e))
-}
-
 module.exports = {
   publish,
-  emit,
 }
